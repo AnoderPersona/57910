dd5794535fc90b33c516be2f1faeaeb6b7d046c3
AnoderPersona <tamibadilla@gmail.com>
2024-02-29 23:12:16 -0300

    ñ

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import './firebase/config'
-// import firebase from 'firebase/app';
-import { getFirestore, doc, getDoc } from "firebase/firestore";
 import 'firebase/firestore';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
